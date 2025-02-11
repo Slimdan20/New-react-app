b5c7030e76e40980afe8665da5f4f7bb116392bc
Daniel Efe <daneytechwriter@gmail.com>
2025-02-11 09:05:05 -0800

    new commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+describe('Percy Visual Test', () => {
+   it('should take a Percy snapshot', () => { 
+    cy.visit('http://localhost:5173'); // Ensure your React app is running 
+    cy.percySnapshot('Homepage'); 
+  }); 
+});
